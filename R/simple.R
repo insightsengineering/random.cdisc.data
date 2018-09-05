2e9e23c4d1b03083a3bc4b0e0bc313bf7b981ff6
wangh107 <wangh107@gene.com>
2018-09-05 15:20:16 -0700

    add racm for commitant medication data

--- R/simple.R
+++ R/simple.R
@@ -229,3 +229,41 @@ raae <- function(ASL, max_n_aes = 10, seed = NULL) {
   )
 }
 
+
+
+lookup_cm = tribble(
+  ~CMCLAS,   ~CMDECOD, ~CMTIREL,
+  "medcl A",   "medname A_1/3",      "PRIOR",
+  "medcl A",   "medname A_2/3",      "CONCOMITANT",
+  "medcl A",   "medname A_3/3",      "CONCOMITANT",
+  "medcl B",   "medname B_1/4",      "CONCOMITANT",
+  "medcl B",   "medname B_2/4",      "PRIOR",
+  "medcl B",   "medname B_3/4",      "PRIOR",
+  "medcl B",   "medname B_4/4",      "CONCOMITANT",
+  "medcl C",   "medname C_1/2",      "CONCOMITANT",
+  "medcl C",   "medname C_2/2",      "CONCOMITANT"
+)
+
+#' @export
+racm <- function(ASL, max_n_cms = 10, seed = NULL) {
+
+  if (!is.null(seed)) set.seed(seed)
+
+  Map(function(id, sid) {
+    n_cms <- sample(0:max_n_cms, 1)
+    i <- sample(1:nrow(lookup_cm), n_cms, TRUE)
+    lookup_cm[i, ] %>% mutate(
+      USUBJID = id,
+      STUDYID = sid
+    )
+  }, ASL$USUBJID, ASL$STUDYID) %>%
+    Reduce(rbind, .) %>%
+    `[`(c(4,5,1,2,3)) %>%
+    var_relabel(
+      STUDYID = "Study Identifier",
+      USUBJID = "Unique Subject Identifier",
+      CMCLAS = "Medication Class",
+      CMDECOD = "Standardized Medication Name",
+      CMTIREL = "Time Relation of Medication"
+    )
+}
