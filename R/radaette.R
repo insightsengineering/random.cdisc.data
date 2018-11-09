36022fee02c7639e4e142c4f437856eef8090bc2
mix <xiuting.mi@roche.com>
2018-11-09 09:38:47 +0100

    adaette dataset

---
+++ R/radaette.R
@@ -0,0 +1,75 @@
+lookup_ADSAFTTE <- tribble(
+  ~ARM,  ~PARAMCD, ~PARAM, ~ LAMBDA, ~CNSR_P,
+  "ARM A", "AETTE1",   "Time to first occurrence of any adverse event",  1/80,     0.4,
+  "ARM B", "AETTE1",   "Time to first occurrence of any adverse event", 1/100,     0.2,
+  "ARM C", "AETTE1",   "Time to first occurrence of any adverse event",  1/60,     0.42,
+  "ARM A", "AETTE2",   "Time to first occurrence of any serious adverse event",  1/100,     0.3,
+  "ARM B", "AETTE2",   "Time to first occurrence of any serious adverse event", 1/150,     0.1,
+  "ARM C", "AETTE2",   "Time to first occurrence of any serious adverse event",  1/80,     0.32,
+  "ARM A", "AETTE3",   "Time to first occurrence of a grade 3-5 adverse event",  1/80,     0.2,
+  "ARM B", "AETTE3",   "Time to first occurrence of a grade 3-5 adverse event", 1/100,     0.08,
+  "ARM C", "AETTE3",   "Time to first occurrence of a grade 3-5 adverse event",  1/60,     0.23
+)
+
+evntdescr_sel <- c(
+  'Preferred Term',
+  'Clinical Cut Off',
+  'Completion or Discontinuation',
+  'End of AE Reporting Period'
+)
+
+cnsdtdscr_sel <- c(
+  'Preferred Term',
+  'Clinical Cut Off',
+  'Completion or Discontinuation',
+  'End of AE Reporting Period'
+)
+
+
+#' Time To Adverse Event Dataset
+#'
+#' @export
+#'
+#' @examples
+#'
+#' library(tern)
+#' radam("adaette")
+#' asl <- radam("ASL")
+#' radaette(ASL = asl)
+#'
+
+radaette <- function(ASL, seed = NULL) {
+
+  if (!is.null(seed)) set.seed(seed)
+
+  # pinfo <- split(ASL, ASL$USUBJID)[[1]]
+  split(ASL, ASL$USUBJID) %>% lapply(FUN = function(pinfo) {
+
+    lookup_ADSAFTTE %>% filter(ARM == as.character(pinfo$ARM)) %>%
+      rowwise() %>%
+      mutate(
+        STUDYID = pinfo$STUDYID,
+        #SITEID = pinfo$SITEID,
+        USUBJID = pinfo$USUBJID,
+        CNSR = sample(c(0, 1), 1, prob = c(1-CNSR_P, CNSR_P)),
+        AVAL = rexp(1, LAMBDA) / 365.25,
+        AVALU = "YEARS",
+        EVNTDESC = if (CNSR == 0) sample(evntdescr_sel, 1) else "",
+        CNSDTDSC = if (CNSR == 1) sample(cnsdtdscr_sel, 1) else ""
+      ) %>% select(-LAMBDA, -CNSR_P)
+
+  })  %>% Reduce(rbind, .) %>%
+    var_relabel(
+      STUDYID = "Study Identifier",
+      USUBJID = "Unique Subject Identifier",
+      #SITEID = "Study Site Identifier",
+      PARAM = "Parameter Description",
+      PARAMCD = "Parameter Code",
+      AVAL = "Analysis Value",
+      AVALU = "Analysis Value Unit",
+      EVNTDESC = "Event Description",
+      CNSDTDSC = "Censor Date Description",
+      CNSR = "Censoring Status Value(1=cens, 0=evt)"
+    )
+
+}
