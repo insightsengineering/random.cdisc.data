685afc419eb797c4ee819f5a5dd3f1cd65c9bdfc
imazubi <imanol.zubizarreta@businesspartner.roche.com>
2021-11-17 11:58:19 +0100

    work in progress

--- R/radpc.R
+++ R/radpc.R
@@ -1,11 +1,11 @@
 
 library(dplyr)
-ADSL <- radsl(N = 10, seed = 1, study_duration = 2)
+library(forcats)
+ADSL <- radsl(N = 100, seed = 1, study_duration = 2)
 
 radpc <- function(
   ADSL,
   avalu = "ug/mL",
-  reltmu = "hr",
   timepoints = c(0, 0.5, 1, 1.5 , 2)
 
 ){
@@ -13,8 +13,8 @@ radpc <- function(
   ADPC <- expand.grid( # nolint
     STUDYID = unique(ADSL$STUDYID),
     USUBJID = ADSL$USUBJID,
-    PCPTNUM = timepoints,
-    PARAM = paste0(c("Plasma Drug X", "Plasma Drug Y"), "(", avalu, ")"),
+    PARAM = factor(paste0(c("Plasma Drug X", "Plasma Drug Y"), "(", avalu, ")")),
+    PCPTNUM = c(0, 0.5, 1, 1.5, 2, 3, 4, 8, 12, 24),
     stringsAsFactors = FALSE
   )
 
@@ -22,26 +22,57 @@ radpc <- function(
     inner_join(ADPC, by = c("STUDYID", "USUBJID"))
   ADPC <- ADPC %>%
     mutate(
-      PCPT = factor(case_when(
+      VISITDY = case_when(
+        PCPTNUM < 24 ~ 1,
+        PCPTNUM >=24 & PCPTNUM < 48 ~ 2,
+        PCPTNUM == 48 ~ 3,
+        PCPTNUM == 72 ~ 4,
+        TRUE ~ 8
+      ),
+      VISIT = paste("Day", VISITDY),
+      PCPT = fct_reorder(factor(case_when(
         PCPTNUM == 0 ~ "Predose",
         TRUE ~ paste0(PCPTNUM, "H")
       )
+      ), PCPTNUM),
+      NRELTM1 = PCPTNUM,
+      ARELTM1 = round(rnorm(n(), mean = PCPTNUM, sd = 0.1), 1),
+      is_plasmay = case_when(
+        grepl("Plasma Drug Y", ADPC$PARAM) ~ TRUE,
+        TRUE ~ FALSE,
       ),
-      AVAL = ifelse(
-        ARMCD == "ARM B",  NA,
-        ifelse(PCPTNUM == 0,  0,
-               ifelse(grepl("Plasma Drug X", ADPC$PARAM),
-                      rnorm(n(), 50, 5) + PCPTNUM * rnorm(n(), 25, 5),
-                      ifelse(grepl("Plasma Drug Y", ADPC$PARAM) & ARMCD == "ARM A",
-                                    NA,
-                                    rnorm(n(), 80, 5) + PCPTNUM * rnorm(n(), 40, 5))
-                             )
-                      )
-
+      substract = case_when(
+        PCPTNUM == 3 ~ 2.5,
+        PCPTNUM == 4 ~ 3,
+        PCPTNUM == 8 ~ 3.5,
+        PCPTNUM == 12 ~ 4,
+        TRUE ~ 0
+      ),
+      AVAL = case_when(
+        ARMCD == "ARM B" | (ARMCD == "ARM A" & grepl("Plasma Drug Y", PARAM)) ~ NA_real_,
+        PCPT == "Predose" |  PCPTNUM == 24 ~ 0,
+        TRUE ~ round(
+          rnorm(n(), 50, 5) +  PCPTNUM * rnorm(n(), 25, 1) + is_plasmay * rnorm(n(), 25, 5) - substract * rnorm(n(), 25, 1),
+          2
+        )
+      ),
+      AVALC = factor(case_when(
+        AVAL == NA_real_ ~ "NA",
+        AVAL == 0 ~ "BLQ",
+        TRUE ~ as.character(round(AVAL, 0))
       )
-
-    )
+      ),
+      AVALU = avalu,
+      RELTMU = "hr"
+    ) %>%
+    select(-c(is_plasmay, substract))
 
 }
 
 
+a <- split(ADPC, ADPC$PARAM)
+
+sum <- lapply(a, summary)
+
+
+ggplot(data = ADPC, aes(x = PCPT, y = AVAL, fill = PARAM)) + geom_boxplot()
