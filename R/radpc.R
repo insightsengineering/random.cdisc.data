71535262c46845d2fd6838ff0bae43690db52787
imazubi <imanol.zubizarreta@businesspartner.roche.com>
2021-11-22 14:10:18 +0100

    improvements made

--- R/radpc.R
+++ R/radpc.R
@@ -1,9 +1,10 @@
 #' PK dataset
 #'
-#' @param ADSL
+#' @template ADSL_params
 #' @param avalu (`string`)\cr Analysis value unit value
-#' @param rel_increase_by_visit (`numeric`)\cr relative increase in AVAL over visits.
-#' @param rel_increase_by_plasma (`numeric`)\cr relative increase in AVAL between parameters.
+#' @param base_mean (`numeric`)
+#' @param rel_increase_by_visit (`numeric`)\cr relative increase in analysis value over visits.
+#' @param rel_increase_by_plasma (`numeric`)\cr relative increase in analysis value between parameters.
 #' @inheritParams radsl
 #'
 #' @details One record per per study per subject per parameter per time point
@@ -16,7 +17,7 @@
 #' library(random.cdisc.data)
 #' library(dplyr)
 #' ADSL <- radsl(N = 10, seed = 1, study_duration = 2)
-#' ADPC <- radpc(ADSL)
+#' ADPC <- radpc(ADSL, seed = 2)
 #'
 radpc <- function(
   ADSL, # nolint
@@ -25,6 +26,10 @@ radpc <- function(
   rel_increase_by_visit = 0.5,
   rel_increase_by_plasma = 0.5,
   seed = NULL,
+  na_percentage = 0,
+  na_vars = list(
+    AVAL = c(NA, 0.1)
+  ),
   cached = FALSE
 ){
 
@@ -48,45 +53,45 @@ radpc <- function(
     STUDYID = unique(ADSL$STUDYID),
     USUBJID = ADSL$USUBJID,
     PARAM = factor(paste0(c("Plasma Drug X", "Plasma Drug Y"), "(", avalu, ")")),
-    PCPTNUM = c(0, 0.5, 1, 1.5, 2, 3, 4, 8, 12),
+    PCTPTNUM = c(0, 0.5, 1, 1.5, 2, 3, 4, 8, 12),
     stringsAsFactors = FALSE
   )
 
-  ADPC <- ADSL[, c("STUDYID", "USUBJID", "ARMCD", "ARM", "AGE", "SEX")] %>%
+  ADPC <- ADSL %>%
     dplyr::inner_join(ADPC, by = c("STUDYID", "USUBJID"))
   ADPC <- ADPC %>%
-    mutate(
+    dplyr::mutate(
       VISITDY = case_when(
-        PCPTNUM < 24 ~ 1,
-        PCPTNUM >=24 & PCPTNUM < 48 ~ 2,
-        PCPTNUM == 48 ~ 3,
-        PCPTNUM == 72 ~ 4,
+        PCTPTNUM < 24 ~ 1,
+        PCTPTNUM >=24 & PCTPTNUM < 48 ~ 2,
+        PCTPTNUM == 48 ~ 3,
+        PCTPTNUM == 72 ~ 4,
         TRUE ~ 8
       ),
       VISIT = paste("Day", VISITDY),
-      PCPT = factor(case_when(
-        PCPTNUM == 0 ~ "Predose",
-        TRUE ~ paste0(PCPTNUM, "H")
+      PCTPT = factor(case_when(
+        PCTPTNUM == 0 ~ "Predose",
+        TRUE ~ paste0(PCTPTNUM, "H")
         ),
       ),
-      NRELTM1 = PCPTNUM,
-      ARELTM1 = round(rnorm(n(), mean = PCPTNUM, sd = 0.1), 1),
+      NRELTM1 = PCTPTNUM,
+      ARELTM1 = round(rnorm(n(), mean = PCTPTNUM, sd = 0.1), 1),
       is_plasmay = case_when(
         grepl("Plasma Drug Y", ADPC$PARAM) ~ TRUE,
         TRUE ~ FALSE,
       ),
       substract = case_when(
-        PCPTNUM == 3 ~ 2,
-        PCPTNUM == 4 ~ 3.5,
-        PCPTNUM == 8 ~ 9,
+        PCTPTNUM == 3 ~ 2,
+        PCTPTNUM == 4 ~ 3.5,
+        PCTPTNUM == 8 ~ 9,
         TRUE ~ 0
       ),
       AVAL = case_when(
         ARMCD == "ARM B" | (ARMCD == "ARM A" & grepl("Plasma Drug Y", PARAM)) ~ NA_real_,
-        PCPT == "Predose" |  PCPTNUM == 12 ~ 0,
+        PCTPT == "Predose" |  PCTPTNUM == 12 ~ 0,
         TRUE ~ round(
           rnorm(n(), base_mean, 5) +
-            PCPTNUM * rnorm(n(), base_mean * rel_increase_by_visit, 1)
+            PCTPTNUM * rnorm(n(), base_mean * rel_increase_by_visit, 1)
           + is_plasmay * rnorm(n(), base_mean * rel_increase_by_plasma, 5)
           - substract * rnorm(n(), base_mean * rel_increase_by_visit, 0.2),
           2)
@@ -100,6 +105,12 @@ radpc <- function(
       AVALU = avalu,
       RELTMU = "hr"
     ) %>%
-    select(-c(is_plasmay, substract, ARMCD, ARM))
+    dplyr::select(-c(is_plasmay, substract))
 
+  if (length(na_vars) > 0 && na_percentage > 0 && na_percentage <= 1) {
+    ADPC <- mutate_na(ds = ADPC, na_vars = na_vars, na_percentage = na_percentage) # nolint
+  }
+
+  ADPC <- apply_metadata(ADPC, "metadata/ADPC.yml") # nolint
+  return(ADPC)
 }
