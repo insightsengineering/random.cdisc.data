a7a4acc998a5f5f02c58bc356183715f40a78c86
Rosemary Li <liy396@gene.com>
2021-12-06 11:04:14 -0800

    draft of ADPP

--- R/radpp.R
+++ R/radpp.R
@@ -22,9 +22,9 @@ radpc <- function(
   paramcd = c("AUCIFO", "CMAX", "CLO"),
   param = c("AUC Infinity Obs", "Max Conc", "Total CL Obs"),
   paramu = c("day*ug/mL", "ug/mL", "ml/day/kg"),
-
-
-  # constants = c(D = 100, ka = 0.8, ke = 1),
+  aval_mean = c(200, 30, 5),
+  visit_format = "CYCLE",
+  n_days = 2L,
   seed = NULL,
   na_percentage = 0,
   na_vars = list(
@@ -38,64 +38,53 @@ radpc <- function(
     return(get_cached_data("cadlb"))
   }
 
-  stopifnot(
-    is_character_single(avalu),
-    all(names(constants) %in% c("D", "ka", "ke"))
-  )
-
   if (!is.null(seed)) {
     set.seed(seed)
   }
 
-  ADPC <- tidyr::expand_grid( # nolint
-    # nolint
-    data.frame(
-      STUDYID = ADSL$STUDYID,
-      USUBJID = ADSL$USUBJID,
-      ARMCD = ADSL$ARMCD,
-      A0 = unname(constants["D"]),
-      ka = unname(constants["ka"]) - stats::runif(length(ADSL$USUBJID), -0.2, 0.2),
-      ke = unname(constants["ke"]) - stats::runif(length(ADSL$USUBJID), -0.2, 0.2)
-    ),
-    PCTPTNUM = c(0, 0.5, 1, 1.5, 2, 3, 4, 8, 12),
-    PARAM = c("Plasma Drug X", "Plasma Drug Y"),
-  ) %>%
-    dplyr::mutate(
-      VISITDY = dplyr::case_when(
-        .data$PCTPTNUM < 24 ~ 1,
-        .data$PCTPTNUM >= 24 & PCTPTNUM < 48 ~ 2,
-        .data$PCTPTNUM == 48 ~ 3,
-        .data$PCTPTNUM == 72 ~ 4,
-        TRUE ~ 8
-      ),
-      VISIT = paste("Day",  .data$VISITDY),
-      PCTPT = factor(dplyr::case_when(
-        .data$PCTPTNUM == 0 ~ "Predose",
-        TRUE ~ paste0(.data$PCTPTNUM, "H")
-      )),
-      ARELTM1 = .data$PCTPTNUM,
-      NRELTM1 =  .data$PCTPTNUM,
-      A0 = ifelse(.data$PARAM == "Plasma Drug Y", .data$A0,  .data$A0 / 2),
-      AVAL = round((.data$A0 * .data$ka * (
-        exp(-.data$ka * .data$ARELTM1) - exp(-.data$ke * .data$ARELTM1)
-      ))
-      / (.data$ke - .data$ka),
-      digits = 3),
-      # PK Equation
-      AVALC = ifelse(.data$AVAL == 0, "BLQ", as.character(.data$AVAL)),
-      AVALU =  avalu,
-      RELTMU = "hr"
-    ) %>%
-    dplyr::select(-c(.data$A0, .data$ka, .data$ke))
+  # validate and initialize related variables
+  param_init_list <- relvar_init(param, paramcd)
+  unit_init_list <- relvar_init(param, paramu)
 
-  ADPC <- ADSL %>% # nolint
-    dplyr::inner_join(ADPC, by = c("STUDYID", "USUBJID", "ARMCD")) %>% # nolint
-    dplyr::filter(ACTARM != "B: Placebo", !(ACTARM == "A: Drug X" & PARAM == "Plasma Drug Y"))
+  ADPP <- expand.grid(
+    STUDYID = unique(ADSL$STUDYID),
+    USUBJID = ADSL$USUBJID,
+    PPCAT = as.factor(ppcat),
+    PARAM = as.factor(param_init_list$relvar1),
+    AVISIT = visit_schedule(visit_format = visit_format, n_assessments = 1L, n_days = n_days),
+    stringsAsFactors = FALSE
+  )
+  ADPP <- ADPP %>%
+    dplyr::mutate(AVAL = stats::rnorm(nrow(ADPP), mean = 1, sd = 0.2)) %>%
+    dplyr::left_join(data.frame(PARAM = param, ADJUST = aval_mean), by = "PARAM") %>%
+    dplyr::mutate(AVAL = .data$AVAL * .data$ADJUST) %>%
+    dplyr::select(-.data$ADJUST)
+
+  # assign related variable values: PARAMxPARAMCD are related
+  ADPP$PARAMCD <- as.factor(rel_var( # nolint
+    df = ADPP,
+    var_name = "PARAMCD",
+    var_values = param_init_list$relvar2,
+    related_var = "PARAM"
+  ))
+  # assign related variable values: PARAMxPARAMU are related
+  ADPP$AVALU <- as.factor(rel_var( # nolint
+    df = ADPP,
+    var_name = "PARAMU",
+    var_values = unit_init_list$relvar2,
+    related_var = "PARAM"
+  ))
+
+
+  ADPP <- ADSL %>% # nolint %>%
+    dplyr::select(STUDYID, USUBJID, AGE, SEX, RACE, ARM, ARMCD, ACTARM, ACTARMCD) %>%
+    dplyr::inner_join(ADPP, by = c("STUDYID", "USUBJID")) %>% # nolint
+    dplyr::filter(ACTARM != "B: Placebo", !(ACTARM == "A: Drug X" & PPCAT == "Plasma Drug Y"))
 
   if (length(na_vars) > 0 && na_percentage > 0 && na_percentage <= 1) {
-    ADPC <- mutate_na(ds = ADPC, na_vars = na_vars, na_percentage = na_percentage) # nolint
+    ADPP <- mutate_na(ds = ADPP, na_vars = na_vars, na_percentage = na_percentage) # nolint
   }
 
-  ADPC <- apply_metadata(ADPC, "metadata/ADPC.yml") # nolint
-  return(ADPC)
+  ADPP <- apply_metadata(ADPP, "metadata/ADPP.yml") # nolint
+  return(ADPP)
 }
