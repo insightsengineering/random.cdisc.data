a4e67b64a748941c69561c0873d7330129808f66
qiliu1013 <liuq36@gene.com>
2022-02-24 00:20:10 +0000

    add nolint

--- R/radsl.R
+++ R/radsl.R
@@ -191,14 +191,14 @@ radsl <- function(N = 400, # nolint
     dplyr::select(-.data$st_posixn)
 
   # add random ETHNIC (Ethnicity)
-  ADSL<- ADSL %>%
+  ADSL <- ADSL %>% # nolint
     dplyr::mutate(ETHNIC = sample(x =  c("HISPANIC OR LATINO", "NOT HISPANIC OR LATINO", " NOT REPORTED", "UNKNOWN"),
                                   size = N, replace = TRUE, prob = c(.1, .8, .06, .04)))
 
   # associate DTHADY (Relative Day of Death) with Death date
   # Date of Death [ADSL.DTHDT] - date part of Date of First Exposure to Treatment [ADSL.TRTSDTM]
 
-  ADSL <- ADSL %>%
+  ADSL <- ADSL %>% # nolint
     dplyr::mutate(DTHADY = as.numeric(DTHDT - as.Date(adsl$TRTSDTM)))
 
 
