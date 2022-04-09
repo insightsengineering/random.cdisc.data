30b76cd06b263cd12b1db24f96e4a43f49660c26
Rosemary Li <liy396@gene.com>
2022-04-08 21:17:09 -0700

    update unit test for radsl with large N

--- tests/testthat/test_rdata.R
+++ tests/testthat/test_rdata.R
@@ -257,3 +257,12 @@ test_that("metadata matches radhy", {
 
   expect_identical(meta_info, rdf_label)
 })
+
+test_that("radsl works with large N", {
+  N <- 120000
+  adsl <- radsl(N, seed = 2)
+
+  adsl_n <- nrow(adsl)
+
+  expect_equal(adsl_n, N)
+})
