ef5e0dac50c7193aa3668a06408a03f06dc12b42
joe.zhu <joe.zhu@roche.com>
2022-04-28 22:13:14 +0000

    update style

--- R/radpp.R
+++ R/radpp.R
@@ -23,8 +23,10 @@ radpp <- function(ADSL, # nolint,
                   ppcat = c("Plasma Drug X", "Plasma Drug Y"),
                   ppspec = c("Plasma", "Plasma", "Plasma", "Matrix of PD", "Matrix of PD", "Urine", "Urine"),
                   paramcd = c("AUCIFO", "CMAX", "CLO", "RMAX", "TON", "RENALCL", "RENALCLD"),
-                  param = c("AUC Infinity Obs", "Max Conc", "Total CL Obs", "Time of Maximum Response",
-                            "Time to Onset", "Renal CL", "Renal CL Dose Norm"),
+                  param = c(
+                    "AUC Infinity Obs", "Max Conc", "Total CL Obs", "Time of Maximum Response",
+                    "Time to Onset", "Renal CL", "Renal CL Dose Norm"
+                  ),
                   paramu = c("day*ug/mL", "ug/mL", "ml/day/kg", "hr", "hr", "L/hr", "L/hr/mg"),
                   aval_mean = c(200, 30, 5, 10, 3, 0.05, 0.005),
                   visit_format = "CYCLE",
