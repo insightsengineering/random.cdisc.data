ba60f675b641d87c6b097c08495c19e26a3d5550
Michael Thorpe <mthorpe@outlook.com>
2022-05-12 10:52:00 +0000

    #18 styling file and adding lstalvdt catch

--- tests/testthat/test_rdata.R
+++ tests/testthat/test_rdata.R
@@ -268,7 +268,7 @@ test_that("radsl works with large N", {
 })
 
 test_that("radsl has no LSTALVDT greater than DTHDT", {
-adsl <- radsl(cached = TRUE)
-adsl_no_na <- adsl[!is.na(adsl$DTHDT), ]
-expect_true(all(adsl_no_na$DTHDT >= adsl_no_na$LSTALVDT))
+  adsl <- radsl(cached = TRUE)
+  adsl_no_na <- adsl[!is.na(adsl$DTHDT) && !is.na(adsl$LSTALVDT), ]
+  expect_true(all(adsl_no_na$DTHDT >= adsl_no_na$LSTALVDT))
 })
