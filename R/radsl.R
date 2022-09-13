c2ea7f3be07886cca996aed0373edad742faed73
Jia Liu <jia.liu.jl12@roche.com>
2022-09-13 06:49:48 +0000

    add  # nolint

--- R/radsl.R
+++ R/radsl.R
@@ -141,11 +141,11 @@ radsl <- function(N = 400, # nolint
       st_posixn >= quantile(st_posixn)[2] & st_posixn <= quantile(st_posixn)[3] ~ as.POSIXct(NA, origin = "1970-01-01"),
       TRUE ~ TRTEDTM
     )) %>%
-    dplyr::mutate(TRTEDTM = as.POSIXct(.data$TRTEDTM, origin = "1970-01-01"),
+    dplyr::mutate(TRTEDTM = as.POSIXct(.data$TRTEDTM, origin = "1970-01-01"), # nolint
                   AP02EDTM = TRTEDTM,
                   TRT02EDTM = TRTEDTM
-                  ) %>%
-    dplyr::select(-.data$TRTEDTM_discon)
+                  ) %>% # nolint
+    dplyr::select(-.data$TRTEDTM_discon) # nolint
 
   ADSL <- ADSL %>% # nolint
     dplyr::mutate(EOSDT = as.Date(.data$TRTEDTM)) %>%
