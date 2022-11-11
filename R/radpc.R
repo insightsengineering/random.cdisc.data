6801c62be34a1a27bd6c102810ced08b9518a764
legrasv <valentin.legras@roche.com>
2022-11-11 11:44:19 +0000

    styler:::style_active_file()

--- R/radpc.R
+++ R/radpc.R
@@ -69,9 +69,9 @@ radpc <- function(ADSL, # nolint
         PCTPT = factor(dplyr::case_when(
           .data$PCTPTNUM == 0 ~ "Predose",
           (day == 1 & grepl("Urine", .data$PARAM)) ~
-            paste0(lag(.data$PCTPTNUM), "H - ", .data$PCTPTNUM, "H"),
+          paste0(lag(.data$PCTPTNUM), "H - ", .data$PCTPTNUM, "H"),
           (day != 1 & grepl("Urine", .data$PARAM)) ~
-            paste0(as.numeric(.data$PCTPTNUM) - 24, "H - ", .data$PCTPTNUM, "H"),
+          paste0(as.numeric(.data$PCTPTNUM) - 24, "H - ", .data$PCTPTNUM, "H"),
           TRUE ~ paste0(.data$PCTPTNUM, "H")
         )),
         ARELTM1 = .data$PCTPTNUM,
