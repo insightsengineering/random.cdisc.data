7b756e542f6dba5cc0a4ee04c3bff1d49ff335b0
Pawel Rucki <pawelrucki@gmail.com>
2023-02-14 10:44:19 +0100

    one more join to fix

--- R/radpp.R
+++ R/radpp.R
@@ -130,8 +130,8 @@ radpp <- function(ADSL, # nolint,
 
   # derive PPSTINT and PPENINT based on PARAMCD
   T1_T2 <- data.frame(PARAMCD = c("RCAMINT", "RCAMINT", "RCPCINT", "RCPCINT"), PPSTINT = c("P0H", "P0H", "P0H", "P0H"), PPENINT = c("P12H", "P24H", "P12H", "P24H")) # nolint
-  ADPP <- ADPP %>% # nolint %>%
-    dplyr::left_join(T1_T2, by = c("PARAMCD")) # nolint
+  ADPP <- ADPP %>% # nolint
+    dplyr::left_join(T1_T2, by = c("PARAMCD"), multiple = "all")
 
   ADPP <- dplyr::inner_join(ADPP, ADSL, by = c("STUDYID", "USUBJID")) %>% # nolint
     dplyr::filter(.data$ACTARM != "B: Placebo", !(.data$ACTARM == "A: Drug X" &
